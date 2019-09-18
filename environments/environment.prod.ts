c03d3df5d9ad3b3991fe1fb8623095fd92719014
Kristopher Pamplona Ong <ongkristopher@gmail.com>
2019-09-18 16:39:44 +0800

    Add files via upload
    initial commit

---
+++ environments/environment.prod.ts
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+export const environment = {
+  production: true,
+  erp_link: 'http://nbs-kpo:1991',
+  backend_url: 'http://nbs-kpo:4201/',
+  ws_url: 'http://nbs-kpo:4201/',
+};
